d582402ab25129c6d82d89a1f40e1568d61eeb34
henrietta jeansson <henriettajeansson@gmail.com>
2024-07-22 21:05:08 +0200

    add hamburger menu component

--- portfolio-client/src/components/nav.tsx
+++ portfolio-client/src/components/nav.tsx
@@ -1,19 +1,13 @@
 import { Link } from 'react-router-dom';
 import "../styles/navbar.scss";
+import { HamburgerToggle } from './hamburgerMenu';
 
 export const Nav = () => {
 
-    return (<div className="nav-container">
-        <nav className="nav-container__flex">
-            <ul className="nav-container__flex__list">
-                <li className="nav-container__flex__list__item">
-                    <Link to="/" className="nav-container__flex__list__item__link">Hem</Link></li>
-                <li className="nav-container__flex__list__item">
-                    <Link to="/projects" className="nav-container__flex__list__item__link">Projekt</Link></li>
-                <li className="nav-container__flex__list__item">
-                    <Link to="/about" className="nav-container__flex__list__item__link">Om</Link></li>
+    return (<div className="nav">
+        <nav>
 
-            </ul>
+            < HamburgerToggle />
         </nav>
 
     </div>
