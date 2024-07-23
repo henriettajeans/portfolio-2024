6761d43b4f2793eade0b1c8b5ccc48bd7458ebb5
henrietta jeansson <henriettajeansson@gmail.com>
2024-07-23 17:59:22 +0200

    update questions array - now the answer is showing in a section undeneath the questions

--- portfolio-client/src/components/aboutme.tsx
+++ portfolio-client/src/components/aboutme.tsx
@@ -3,43 +3,39 @@ import { IQuestionAndAnswers, questionAndAnswers } from "../model/questionsList"
 
 export const About = () => {
 
-    const [list, setList] = useState<IQuestionAndAnswers[]>(questionAndAnswers)
-    const [toggleAnswer, setToggleAnswer] = useState<{ [key: string]: boolean }>({})
+    const [list, setList] = useState<IQuestionAndAnswers[]>(questionAndAnswers);
+    const [openQuestionId, setOpenQuestionId] = useState<string | null>(null);
 
     const handleToggle = (id: string) => {
-        setToggleAnswer(prevState => ({
-            ...prevState,
-            [id]: !prevState[id]
-        }))
+        if (openQuestionId === id) {
+            setOpenQuestionId(null); // Close the currently open question if it's clicked again
+        } else {
+            setOpenQuestionId(id); // Open the clicked question
+        }
     }
 
     let questionsListHtml = list.map(question => {
-        const selectId = list.map(({ id }) => id)
-        console.log(selectId);
-
         return (
-            <>
-                <div key={question.id}>
-                    <h5 onClick={() => handleToggle(question.id)}> {question.question}
-
-                    </h5>
+            <div key={question.id}>
+                <div>
+                    <h5 onClick={() => handleToggle(question.id)}> {question.question} </h5>
                 </div>
-
-                <section>
-                    {toggleAnswer[question.id] &&
-                        <p >
-                            {question.answer}
-                        </p>
-                    }
-                </section>
-            </>
+            </div>
         )
-    })
+    });
+
+    const selectedQuestion = list.find(question => question.id === openQuestionId);
+
     return (
         <div className="about-container" >
             <h2 className="about-container__title">Lite om mig</h2>
             <section>
                 {questionsListHtml}
+            </section>
+            <section>
+                {selectedQuestion && (
+                    <p>{selectedQuestion.answer}</p>
+                )}
             </section>
         </div>
     )
