bb7c3517cd302feae4281ee2b4862aafb14c02a5
henrietta jeansson <henriettajeansson@gmail.com>
2024-07-23 14:12:53 +0200

    add qna list to about me page

--- portfolio-client/src/components/aboutme.tsx
+++ portfolio-client/src/components/aboutme.tsx
@@ -1,16 +1,38 @@
+import { useState } from "react";
+import { IQuestionAndAnswers, questionAndAnswers } from "../model/questionsList";
 
 export const About = () => {
+
+    const [list, setList] = useState<IQuestionAndAnswers[]>(questionAndAnswers)
+
+
+    // const myJSON = JSON.stringify(qnaList)
+    // console.log(qnaList)
+    // console.log(myJSON)
+
     return (
         <div className="about-container" >
             <h1 className="about-container__title">Lite om mig</h1>
 
             <div className="about-container__wrapper__line"></div>
-            <p className="about-container__wrapper__desc">
-                Som student vid Medieinstitutet strävar jag efter att utveckla mina färdigheter för att bli en framgångsrik webbutvecklare.
-                Som person är jag social och trevlig, alltid öppen för nya idéer och tekniker och arbetar hårt för att förbättra mig själv och bygga upp min portfölj.
-                Med en positiv inställning och engagemang inom webbutveckling ser jag ljust på min framtid inom området.
-            </p>
-            <div className="about-container__wrapper__line"></div>
+
+            <section>
+                {
+                    list.map(item => {
+                        return (
+                            <div className={'App-item'}>
+                                <div><h5> {item.question}</h5> </div>
+
+                                <p className={'Price-item'}>
+                                    ! {item.answer}
+                                </p>
+                            </div>
+                        )
+
+                    })
+
+                }
+            </section>
 
         </div>
     )
---
+++ portfolio-client/src/model/questionsList.tsx
@@ -0,0 +1,30 @@
+
+export interface IQuestionAndAnswers {
+    id: string
+    question: string
+    answer: string
+}
+
+export const questionAndAnswers: IQuestionAndAnswers[] =
+    [
+        {
+            id: '0',
+            question: 'Varför gjorde du så?',
+            answer: "Jag vill du göra nåt annat"
+        },
+        {
+            id: '1',
+            question: 'Varför gjorde du så?',
+            answer: "Jag vill du göra nåt annat"
+        },
+        {
+            id: '2',
+            question: 'Varför gjorde du så?',
+            answer: "Jag vill du göra nåt annat"
+        },
+        {
+            id: '3',
+            question: 'Varför gjorde du så?',
+            answer: "Jag vill du göra nåt annat"
+        },
+    ]
