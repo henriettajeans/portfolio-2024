fdb9eac6f7c62172a81a56211d87e39bc0dd12fd
henrietta jeansson <henriettajeansson@gmail.com>
2024-08-07 17:17:44 +0200

    small commit some fixes

--- portfolio-client/src/model/filterType.tsx
+++ portfolio-client/src/model/filterType.tsx
@@ -1,4 +1,4 @@
-export type Topics = "php" | "typescript" | "frontend" | "backend" | "wordpress"
+export type Topics = "php" | "typescript" | "frontend" | "backend" | "wordpress" | "node" | "api"
 
 export type GroupProject = "Yes" | "No"
 
@@ -6,4 +6,4 @@ export type HobbyProject = "Yes" | "No"
 
 export type SortOption = "asc" | "desc"
 
-export const TOPICS: Topics[] = ["php", "typescript", "frontend", "backend", "wordpress"];
+export const TOPICS: Topics[] = ["php", "typescript", "frontend", "backend", "wordpress", "node", "api"];
