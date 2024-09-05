285b54160c34049ad47fb1c2e927af9b1e210477
henrietta jeansson <henriettajeansson@gmail.com>
2024-09-05 19:58:43 +0200

    update single page display with icons and styling

--- portfolio-client/src/components/singleProject.tsx
+++ portfolio-client/src/components/singleProject.tsx
@@ -3,7 +3,8 @@ import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { IRepo } from "../model/IProject";
 import { GetRepoById } from "../controller/getProject";
-import { RiArrowGoBackLine } from "react-icons/ri";
+import { RiArrowGoBackFill } from "react-icons/ri";
+import { VscGithub } from "react-icons/vsc";
 
 
 
@@ -30,7 +31,7 @@ export const Project = (() => {
     return (<>
         <article className="project-component">
             {/* TODO: Move button fither down to make it clickable (margin hinders this) */}
-            <button onClick={getBack} className="singleProjectBtn"> <RiArrowGoBackLine /> </button>
+            <button onClick={getBack} className="singleProjectBtn"> <RiArrowGoBackFill /> </button>
             {/* TODO: Add next project button */}
             <h2 className="project-component__title">{project?.name}</h2>
             <div className="project-component__flex">
@@ -38,7 +39,7 @@ export const Project = (() => {
                 <h4 className="project-component__flex__desctitle">Beskrivning av projektet</h4>
                 <p className="project-component__flex__desc">{project?.description}</p>
                 <a href={project?.html_url} className="project-component__flex__url__link">
-                    ~ Se repot på github ~
+                    Se repot på github <VscGithub />
                 </a>
                 <h4>I projektet har följade tekniker använts: </h4>
                 <span className="project-component__flex__desc"> {project?.topics?.join(", ")}</span>
