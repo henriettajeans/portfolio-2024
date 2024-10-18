84e616be43f3ff17a6086f8ce0b88c4684199caf
henrietta jeansson <henriettajeansson@gmail.com>
2024-10-18 17:30:31 +0200

    update cleanup in header component and children

--- portfolio-client/src/components/header.tsx
+++ portfolio-client/src/components/header.tsx
@@ -1,12 +1,11 @@
 import '../styles/header.scss';
 import { FC } from 'react';
-import { HamburgerToggle } from './hamburgerMenu';
 
 
 const Header: FC = () => {
 
     return (
-        < HamburgerToggle />
+        <></>
 
     );
 };
