f8601a1f29fd7e5749cbf758761e7fbe5bf0c289
henrietta jeansson <henriettajeansson@gmail.com>
2024-10-31 13:12:39 +0100

    add about component

---
+++ portfolio-client/src/components/about.tsx
@@ -0,0 +1,16 @@
+export const About = () => {
+
+    return (
+        <><h2>Henrietta Jeansson</h2>
+            <span>
+
+            </span>
+            <span>
+
+            </span>
+            <span>
+
+            </span>
+        </>
+    );
+};
--- portfolio-client/src/components/home.tsx
+++ portfolio-client/src/components/home.tsx
@@ -2,6 +2,7 @@ import { QuestionComponent } from './questionComponent';
 import { HeroSection } from './hero';
 import { ResumeComponent } from './resume';
 import { Link } from 'react-router-dom';
+import { About } from './about';
 export const Home = () => {
     return (
         <>
@@ -9,6 +10,7 @@ export const Home = () => {
                 <Link to="/projects" className="nav-container__flex__list__item__link" >Projekt</Link>
             </p>
             <HeroSection />
+            <About />
             <ResumeComponent />
             <QuestionComponent />
 
